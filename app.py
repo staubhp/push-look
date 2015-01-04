a89bcd10f840ed3c22fe161665916d218d44bf45
Payton Staub <staubhpa@gmail.com>
2015-01-04 18:23:01 -0500

    Organized app.py

--- app.py
+++ app.py
@@ -1,29 +1,56 @@
 from github import Github
-
-g = Github()
-x = g.search_repositories("tetris")
-
-d = x[0].description
-dl = x[0].downloads_url
-n = x[0].full_name
-gl = x[0].git_url
-_id = x[0].id
-owner = x[0].owner #returns a NamedUser object
-updt = x[0].updated_at
-url = x[0].url
+from datetime import datetime,timedelta
+from urllib.request import urlretrieve
+import uuid
 
 
-c = x[0].get_commits()
-c[0] #returns a Commit object
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        print(s)
+        if readsofar >= totalsize: # near the end
+            print("\n")
+    else: # total size is unknown
+        print("read %d\n" % (readsofar,))
 
-#this rate limiting stuff is different for search
-#the lib doesn't seem to support seeing your search rate limit
-#might consider adding it
-#https://developer.github.com/v3/rate_limit/
-rl = g.get_rate_limit() #returns a RateLimit object
-r = rl.rate #returns a Rate object
-r.remaining #remaining calls
-r.reset #date time of reset? 
-r.limit #limit of calls
+def getRecentRepos(searchTerm):
+        return ghub.search_repositories(searchTerm, "updated", "desc")
+
+def getRecentCommits(repo):
+        d1 = datetime.today() - timedelta(days=1)
+        d2 = datetime.today()
+        return repo.get_commits(since=d1, until=d2)
+
+def downloadCommitFiles(commit):
+        #TODO: handle this in different module
+        commitFiles = commit.files
+        for i in range(0, len(commitFiles)):
+                print("Downloading " + commitFiles[i].raw_url)
+                filename = 'downloads/' + str(uuid.uuid4()) + ".txt"
+                urlretrieve(commitFiles[i].raw_url, filename, reporthook)
+
+def search():
+        repos = getRecentRepos("tetris")
+        commits = getRecentCommits(repos[0]) 
+        for commit in commits:
+                downloadCommitFiles(commit)
 
 
+
+if __name__ == "__main__":        
+        ghub = Github()
+        search()
+
+
+#TODO: limit on number of commits downloadable
+#TODO: limit on file size of commits willing to download
+#TODO: target specific file types
+#TODO: use code search if rate is up, otherwise download files for manual search
+#TODO: make sure most recent commits are download
+#TODO: move downloader into separate module
+#TODO: create logging
+#TODO: think of program flow. run search in infinite loop, pull from a list of keywords
+        #provided as main input
